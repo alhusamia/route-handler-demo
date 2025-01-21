d023104d17de6fa86c993cf46b848705129af011
Yazan Alhusamia <yalhusamia.c@neoleap.com.sa>
2025-01-21 14:09:15 +0300

    Headers in route handler

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,3 +1,16 @@
-export default function GET() {
-  return new Response("Profile api data");
+import { headers } from "next/headers";
+import { type NextRequest } from "next/server";
+
+export async function GET(request: NextRequest) {
+  // const requestHeader = new Headers(request.headers);
+  // console.log(requestHeader.get("Authorization"));
+
+  const headersList = await headers();
+  console.log(headersList.get("Authorization"));
+
+  return new Response("<h1>Profile api data<h1/>", {
+    headers: {
+      "Content-Type": "text/html",
+    },
+  });
 }
